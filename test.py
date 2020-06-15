ac7491b0be0726ef7c200d58cec6f691119c0d36
pachinko2821 <pachinko28@protonmail.com>
2020-06-15 15:58:17 +0530

    updated file

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 # usage
 
+import sqlite3
 import sys
 from cms import check_profanity, txt2html, show_cuss_words, docx2html
+
+connect = sqlite3.connect("cms.db")
+cursor = connect.cursor()
 
 whateverFile = sys.argv[1] # take txt file as argument    
 
 
-profanity = check_profanity(whateverFile) # check txt file for cuss words, returns True if cuss words found 
-
+profanity, fdetails = check_profanity(whateverFile) # check txt file for cuss words, returns True if cuss words found and file details regardless
+print(fdetails)
+Date, Author, Title = fdetails[0], fdetails[1], fdetails[2]
 
 if profanity == False:
     #for text file
@@ -18,3 +23,7 @@ if profanity == False:
 else:
     print("contains inappropriate words!")
     show_cuss_words() # shows all found cuss words 
+
+cursor.execute(f"INSERT INTO postDetails(Date, Author, Title, Profanity) VALUES('{Date}', '{Author}', '{Title}', '{profanity}');")
+connect.commit()
+connect.close()
