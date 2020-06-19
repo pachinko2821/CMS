c7c9195998bd5019bd4800883498d94d2d55b534
pachinko2821 <pachinko28@protonmail.com>
2020-06-19 23:22:59 +0530

    fixed bugs

--- cmsadmin/change-password.php
+++ cmsadmin/change-password.php
@@ -62,26 +62,26 @@ $result = $statement->execute();
 
 $row = $result->fetchArray(SQLITE3_ASSOC);
 
-if(!empty($old_pass) && !empty($new_pass) && !empty($c_new_pass)){
-if($old_pass == $row['password']){
-    if($new_pass == $c_new_pass){
+if(!isset($old_pass) && !isset($new_pass) && !isset($c_new_pass)){
+    if($old_pass == $row['password']){
+        if($new_pass == $c_new_pass){
 
-        $statement = $db->prepare('UPDATE admins set password=? WHERE password=?;');
-        $statement->bindValue(1, $new_pass);
-        $statement->bindValue(2, $old_pass);
-        $result = $statement->execute();
+            $statement = $db->prepare('UPDATE admins set password=? WHERE password=?;');
+            $statement->bindValue(1, $new_pass);
+            $statement->bindValue(2, $old_pass);
+            $result = $statement->execute();
 
-        echo "<script>alert('Password Updated!')</script>";
-        header("Location: dashboard.php");
-    }
-    else{
-        echo "<script>alert('New password and Confirmation dont match!')</script>";
-    }
+            echo "<script>alert('Password Updated!')</script>";
+            header("Location: dashboard.php");
+        }
+        else{
+            echo "<script>alert('New password and Confirmation dont match!')</script>";
+        }
 
-}
-    else{
-        echo "<script>alert('Wrong Current Password!')</script>";
     }
+        else{
+            echo "<script>alert('Wrong Current Password!')</script>";
+        }
 
 }
 ?>
