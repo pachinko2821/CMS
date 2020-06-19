37ea6f56c03860739fd29763702a81d441ccdca4
pachinko2821 <pachinko28@protonmail.com>
2020-06-19 23:28:00 +0530

    fixed bugs

--- cmsadmin/viewer.php
+++ cmsadmin/viewer.php
@@ -11,6 +11,12 @@ $file = $_GET['file'];
 echo file_get_contents($file);
 
 ?>
+<head>
+    <link rel="stylesheet" type="text/css" href='/includes/css/main.css'>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
 
 <form action="approval.php?file=<?php echo $file; ?>" method="POST">
     <input type="submit" value="approve" name="decision">
